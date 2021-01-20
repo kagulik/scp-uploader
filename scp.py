b42f2ccb153685fbc822a5a5136e03ab5770b4c0
kagulik <tacheza@protonmail.com>
2021-01-20 17:13:08 +0200

    Upload multiple files by 1 ssh connection

--- scp.py
+++ scp.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import sys
 import subprocess
 from decouple import config
@@ -7,7 +8,8 @@ from decouple import config
 SSH = config('SSH', default=False)
 LOCAL_REPO = config('LOCAL_REPO', default=False)
 REMOTE_REPO = config('REMOTE_REPO', default=False)
-if not SSH or not LOCAL_REPO or not REMOTE_REPO:
+REMOTE_FOLDER = config('REMOTE_FOLDER', default=False)
+if not SSH or not LOCAL_REPO or not REMOTE_REPO or not REMOTE_FOLDER:
     sys.exit('Set env vars first.')
 
 # inspect ssh connection
@@ -15,7 +17,10 @@ not_connected = subprocess.run(['ssh', SSH, 'whoami'], stdout=subprocess.DEVNULL
 if not_connected.returncode:
     sys.exit("Unable to establish ssh connection.")
 
+# replace ~ with full path
+LOCAL_REPO = os.path.expanduser(LOCAL_REPO)
 
+# copy files to folder with recreating directory structure
 files = open("files", "r")
 for file in files:
     file = file.strip()
@@ -23,12 +28,19 @@ for file in files:
     if not file:
         continue
 
-    os.system(f"scp {LOCAL_REPO}{file} {SSH}:{REMOTE_REPO}{file}")
+    os.makedirs(os.path.dirname(f"folder/{file}"), exist_ok=True)
+    shutil.copy(f"{LOCAL_REPO}{file}", f"folder/{file}")
 
 files.close
 
+# rename folder to REMOTE_FOLDER
+os.rename('folder', REMOTE_FOLDER)
 
-# os.system(f"scp -r /home/karulka/Documents/co/python/ftp-uploader/folder {SSH}:{REMOTE_REPO}")
-# clear folder
-# copy files to folder with recreating directory structure
 # scp folder
+result = os.system(f"scp -r {REMOTE_FOLDER} {SSH}:{REMOTE_REPO}")
+
+# clear folder
+shutil.rmtree(REMOTE_FOLDER)
+
+# create empty folder
+os.mkdir('folder')
